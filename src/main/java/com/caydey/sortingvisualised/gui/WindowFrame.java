0306a14d611d39e1379d7b4cd5e38a899d28cb92
caydey <caydey@protonmail.com>
2020-11-23 10:55:48 +0000

    Color swapped & compared elements during animation

--- src/main/java/com/caydey/sortingvisualised/gui/WindowFrame.java
+++ src/main/java/com/caydey/sortingvisualised/gui/WindowFrame.java
@@ -33,7 +33,7 @@ public class WindowFrame extends JFrame {
 
 
     // start sorting in thread so it can sleep and run in parrallel
-    Runnable runnable = new SortArray(trackedArray, new QuickSort());
+    Runnable runnable = new SortArray(trackedArray, new CombSort());
     Thread thread = new Thread(runnable);
     thread.start();
 
--- src/main/java/com/caydey/sortingvisualised/gui/arraypanel/ArrayPanel.java
+++ src/main/java/com/caydey/sortingvisualised/gui/arraypanel/ArrayPanel.java
@@ -10,13 +10,22 @@ public class ArrayPanel extends JPanel implements ArrayOperationListener {
   private TrackedArray trackedArray;
   private final int scale;
 
+  private int[] lastCompared = new int[2]; // the 2 elements that were last compared with eachother
+  private int[] lastSwaped = new int[2]; // the 2 elements that were last swaped
+
+
   @Override
   public void swapAction(int indexA, int indexB) {
+    lastSwaped[0] = indexA;
+    lastSwaped[1] = indexB;
     repaint();
-    // System.out.println("swap");
+    System.out.println("swap");
   }
   @Override
   public void compareAction(int indexA, int indexB) {
+    lastCompared[0] = indexA;
+    lastCompared[1] = indexB;
+    repaint();
     System.out.print("compare");
   }
   @Override
@@ -45,6 +54,11 @@ public class ArrayPanel extends JPanel implements ArrayOperationListener {
     int[] array = trackedArray.getArray();
     for (int i=0; i<array.length; i++) {
       g2d.setColor(Color.BLACK);
+      if (i == lastCompared[0] || i == lastCompared[1]) {
+        g2d.setColor(Color.BLUE);
+      } else if (i == lastSwaped[0] || i == lastSwaped[1]) {
+        g2d.setColor(Color.RED);
+      }
       g2d.fillRect(i*scale,500-((array[i]+1)*scale), scale,((array[i]+1)*scale));
     }
   }
