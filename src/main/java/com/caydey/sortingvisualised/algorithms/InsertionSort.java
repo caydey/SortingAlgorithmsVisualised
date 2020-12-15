4f7179239a56c809013bb463417b91c29c716ed1
caydey <caydey@protonmail.com>
2020-12-15 20:14:08 +0000

    Better implementation of Insertion Sort

--- src/main/java/com/caydey/sortingvisualised/algorithms/InsertionSort.java
+++ src/main/java/com/caydey/sortingvisualised/algorithms/InsertionSort.java
@@ -5,12 +5,17 @@ import com.caydey.sortingvisualised.array.TrackedArray;
 public class InsertionSort implements SortingAlgorithm {
   @Override
   public void sort(TrackedArray array) {
-    for (int i=1; i<array.length; i++) {
-      for (int j=i; j>0; j--) {
-        if (array.compareIndexes(j, j-1) < 0) { // j < j-1
-          array.swap(j, j-1);
-        }
+    for (int i=1; i<array.length; ++i) {
+      int key = array.get(i);
+      int j = i-1;
+
+      array.logComparedIndexes(j,i);
+      while (j>=0 && array.get(j) > key) {
+        array.logComparedIndexes(j,i);
+        array.set(j+1, array.get(j));
+        j--;
       }
+      array.set(j+1, key);
     }
   }
 }
